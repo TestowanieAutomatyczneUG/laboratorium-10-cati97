4af60877bf0451f98bdc9017abe82b366a56b686
Katarzyna Siwiec <katasiw97@gmail.com>
2020-12-08 17:37:54 +0100

    test_get_note - green

--- tests/test_note.py
+++ tests/test_note.py
@@ -9,5 +9,8 @@ class TestNote(unittest.TestCase):
     def test_get_name(self):
         self.assertEqual(self.note.get_name, "kolokwium")
 
+    def test_get_note(self):
+        self.assertEqual(self.note.get_note, 4)
+
     def tearDown(self) -> None:
         self.note = Note
