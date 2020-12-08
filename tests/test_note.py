11caed3a0f61c3ef72505f2ec4f24227509b9233
Katarzyna Siwiec <katasiw97@gmail.com>
2020-12-08 17:42:52 +0100

    test_note_out_of_range_1 - green

--- tests/test_note.py
+++ tests/test_note.py
@@ -18,5 +18,8 @@ class TestNote(unittest.TestCase):
     def test_name_is_empty_raises_exception(self):
         self.assertRaises(ValueError, Note, "", 4)
 
+    def test_note_out_of_range_1(self):
+        self.assertRaises(ValueError, Note, "kolokwium", 1)
+
     def tearDown(self) -> None:
         self.note = Note
