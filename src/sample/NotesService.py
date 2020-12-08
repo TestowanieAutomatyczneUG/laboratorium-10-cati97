35333e2e28b2f73cd5e0d8a2f8e8e4fccf9ef026
Katarzyna Siwiec <katasiw97@gmail.com>
2020-12-08 18:00:28 +0100

    NoteStorage and NotesService classes

---
+++ src/sample/NotesService.py
@@ -0,0 +1,19 @@
+from src.sample.NotesStorage import NotesStorage
+
+
+class NotesService:
+    def __init__(self):
+        self.notes_storage = NotesStorage()
+
+    def add(self, note):
+        self.notes_storage.add(note)
+
+    def averageOf(self, name):
+        all_notes = self.notes_storage.getAllNotesOf(name)
+        suma = 0
+        for note in all_notes:
+            suma += note
+        return suma/len(all_notes)
+
+    def clear(self):
+        self.notes_storage.clear()
